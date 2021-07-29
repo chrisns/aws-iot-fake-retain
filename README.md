730a159c581a2ed324fe7be48144c812b1b7b1fe
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-07-29 17:10:16 +0100

    handles wildcards with a scan

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # AWS IoT fake mqtt retain
 
+## Context
+
 > The MQTT specification provides a provision for the publisher to request that the broker retain the last message sent to a topic and send it to all future topic subscribers. AWS IoT doesn't support retained messages. If a request is made to retain messages, the connection is disconnected.
 
 [AWS IoT differences from MQTT version 3.1.1 specification](https://docs.aws.amazon.com/iot/latest/developerguide/mqtt.html#mqtt-differences)
 
 That can be pretty annoying, sometimes you want to retain and recall topic messages without bending to using [IoT Shadows](https://docs.aws.amazon.com/iot/latest/developerguide/iot-device-shadows.html).
 
+## So, whats this?
+
 This workaround requires you to specify a list of topics (or wildcards) that will be stored in a dynamodb table and then when a client subscribes to that topic, it will receive the previous message.
 
-Downsides/caveats:
+## Downsides/caveats
 
 - All topic subscribers will receive the rebroadcast message, there isn't a way to direct to individual subscribers
 - Use it sparingly, actions, lambda and dynamo puts can add up and be expensive
+- every subscription will cost you a full scan of your table
 
-**NOTE: Currently doesn't rebroadcasting to wildcard subscriptions**
+## TODO
+
+- [ ] source endpoint address either dynamically or from env var
+
+## Thanks
+
+I took the mqtt matcher from [ralphtheninja/mqtt-match](https://github.com/ralphtheninja/mqtt-match)
--- index.js
+++ index.js
@@ -1,8 +1,56 @@
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
-const iotdata = new AWS.IotData({ endpoint: process.env.IOT_ENDPOINT })
 
 const TableName = "iotRetain"
+
+module.exports.mqttMatch = function (filter, topic) {
+  const filterArray = filter.split('/')
+  const length = filterArray.length
+  const topicArray = topic.split('/')
+
+  for (var i = 0; i < length; ++i) {
+    var left = filterArray[i]
+    var right = topicArray[i]
+    if (left === '#') return topicArray.length >= length - 1
+    if (left !== '+' && left !== right) return false
+  }
+
+  return length === topicArray.length
+}
+
+module.exports.getTopicsFromSubscriptions = async (subscriptions) => {
+  const params = {
+    TableName,
+    ProjectionExpression: "topic"
+  }
+  let scanResults = []
+  let items
+  do {
+    items = await dynamoDb.scan(params).promise()
+    items.Items.forEach((item) =>
+      scanResults.push(item.topic)
+    )
+    params.ExclusiveStartKey = items.LastEvaluatedKey
+  } while (typeof items.LastEvaluatedKey !== "undefined")
+
+  return scanResults.filter(topic => {
+    return subscriptions.map(filter => module.exports.mqttMatch(filter, topic)).includes(true)
+  })
+}
+
+module.exports.getPayloadsFromTopics = async (topics) =>
+  (await dynamoDb.batchGet({
+    RequestItems: {
+      [TableName]: {
+        Keys: topics.map(topic => {
+          return {
+            topic
+          }
+        })
+      },
+    }
+  }).promise()).Responses[TableName]
+
 
 module.exports.retainer = event =>
   dynamoDb.put({
@@ -13,20 +61,19 @@ module.exports.retainer = event =>
     },
   }).promise()
 
+module.exports.recaller = async event => {
+  const iotdata = new AWS.IotData({ endpoint: process.env.IOT_ENDPOINT })
 
-module.exports.recaller = event =>
-  Promise.all(
-    event.topics.map(topic =>
-      dynamoDb.get({
-        TableName,
-        Key: {
-          topic,
-        },
-      }).promise().then(result =>
-        iotdata.publish({
-          topic: result.Item.topic,
-          payload: JSON.stringify(result.Item.payload),
-        }).promise()
-      )
+  const topics = await module.exports.getTopicsFromSubscriptions(event.topics)
+  if (topics.length === 0) return
+  const messages = await module.exports.getPayloadsFromTopics(topics)
+  if (messages.length === 0) return
+  return Promise.all(
+    messages.map(message =>
+      iotdata.publish({
+        topic: message.topic,
+        payload: JSON.stringify(message.payload),
+      }).promise()
     )
   )
+}
